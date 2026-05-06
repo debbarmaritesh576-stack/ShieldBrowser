05681b739059bfb4a3a84fac546807eab171d6e9
Tab browser <debbarmaritesh576@gmail.com>
2026-05-06 09:09:54 +0530

    Create app/src/main/java/com/shield/browser/data/local/entity/HistoryEntity.kt

---
+++ app/src/main/java/com/shield/browser/data/local/entity/HistoryEntity.kt
@@ -0,0 +1,13 @@
+package com.shield.browser.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "history")
+data class HistoryEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val title: String,
+    val url: String,
+    val dateVisited: Long = System.currentTimeMillis()
+)
