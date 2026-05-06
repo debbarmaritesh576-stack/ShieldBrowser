516106be39d114afa95cfdf141830080f1f009f9
Tab browser <debbarmaritesh576@gmail.com>
2026-05-06 08:52:00 +0530

    Create app/src/main/java/com/shield/browser/extensions/ContextExtensions.kt

---
+++ app/src/main/java/com/shield/browser/extensions/ContextExtensions.kt
@@ -0,0 +1,8 @@
+package com.shield.browser.extensions
+
+import android.content.Context
+import android.widget.Toast
+
+fun Context.showToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
