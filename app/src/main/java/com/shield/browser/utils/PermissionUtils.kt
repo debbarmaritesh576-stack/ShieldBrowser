131179ae5234a7540e6de3aa9e2cae5bed2c4cd1
Tab browser <debbarmaritesh576@gmail.com>
2026-05-06 09:59:15 +0530

    Create app/src/main/java/com/shield/browser/utils/PermissionUtils.kt

---
+++ app/src/main/java/com/shield/browser/utils/PermissionUtils.kt
@@ -0,0 +1,11 @@
+package com.shield.browser.utils
+
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
+
+object PermissionUtils {
+    fun hasPermission(context: Context, permission: String): Boolean {
+        return ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED
+    }
+}
