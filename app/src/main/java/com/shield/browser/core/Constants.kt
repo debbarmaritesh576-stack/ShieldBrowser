fd6199f846dbdc779dab7be28198317963143af1
Tab browser <debbarmaritesh576@gmail.com>
2026-05-06 08:49:38 +0530

    Create app/src/main/java/com/shield/browser/core/Constants.kt

---
+++ app/src/main/java/com/shield/browser/core/Constants.kt
@@ -0,0 +1,10 @@
+package com.shield.browser.core
+
+object Constants {
+    const val DATABASE_NAME = "shield_browser_db"
+    const val PREFS_NAME = "shield_prefs"
+    const val DEFAULT_SEARCH_ENGINE = "google"
+    const val MAX_SUGGESTIONS = 5
+    const val BOOKMARK_EXPORT_FILE = "ShieldBookmarks.json"
+    const val HISTORY_EXPORT_FILE = "ShieldHistory.json"
+}
