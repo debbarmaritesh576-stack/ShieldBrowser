bd6c49eca31b5510c6d1df4675e0f914b65a1a54
Tab browser <debbarmaritesh576@gmail.com>
2026-05-06 09:49:08 +0530

    Create app/src/main/java/com/shield/browser/ui/theme/Color.kt

---
+++ app/src/main/java/com/shield/browser/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.shield.browser.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val ShieldGreen = Color(0xFF00C853)
+val ShieldGreenDark = Color(0xFF009624)
+val DarkBackground = Color(0xFF121212)
+val LightBackground = Color(0xFFFFFFFF)
+val DarkSurface = Color(0xFF1E1E1E)
+val LightSurface = Color(0xFFF5F5F5)
+val DarkTextPrimary = Color(0xFFE0E0E0)
+val LightTextPrimary = Color(0xFF212121)
