9904d7057573fb984e2908429b9b546fe7528ae4
Tab browser <debbarmaritesh576@gmail.com>
2026-05-06 08:50:25 +0530

    Create app/src/main/java/com/shield/browser/core/Urls.kt

---
+++ app/src/main/java/com/shield/browser/core/Urls.kt
@@ -0,0 +1,11 @@
+package com.shield.browser.core
+
+object Urls {
+    const val GOOGLE_SEARCH = "https://www.google.com/search?q="
+    const val DUCKDUCKGO_SEARCH = "https://duckduckgo.com/?q="
+    const val BING_SEARCH = "https://www.bing.com/search?q="
+    const val STARTPAGE_SEARCH = "https://www.startpage.com/sp/search?query="
+    
+    const val GOOGLE_SUGGESTIONS = "https://suggestqueries.google.com/complete/search?client=firefox&q="
+    const val DUCKDUCKGO_SUGGESTIONS = "https://duckduckgo.com/ac/?q=&type=list"
+}
